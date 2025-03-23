619f9c3ffb168a194e9f22a85aa13742ff67e8a3
kyn-317 <kynauto0317@gmail.com>
2025-03-23 19:11:48 +0900

    first commit

---
+++ src/test/java/com/kyn/rsocket_server/RsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyn.rsocket_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
